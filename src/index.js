838dbe5c7af8edd43f6826ab745c05d797890aef
guangtoutou <ni.ningning@gmail.com>
2018-01-13 22:30:30 +0800

    start first Backbone app

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-import jQuery from 'jquery'
+import $ from 'jquery'
+import Backbone from 'backbone'
 
-jQuery( "Body" ).html( "Next Step..." )
+
+var AppRouter = Backbone.Router.extend({
+	routes:{
+		"": "list",
+		"menu-items/new": "itemForm",
+		"menu-items/:item": "itemDetails"
+	},
+
+	list: function(){
+		$('#app').html('List Screen');
+	},
+
+	itemForm: function(){
+		$('#app').html('New item form');
+	},
+
+	itemDetails: function(item){
+		$('#app').html('Menu item:' + item);
+	}
+});
+
+var app = new AppRouter();
+
+$(function(){
+	Backbone.history.start();
+})
