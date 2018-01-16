ea3580ec162edf0650513140f5569789c88ae6ae
guangtoutou <ni.ningning@gmail.com>
2018-01-16 22:20:37 +0800

    use template to escape html

--- src/menuItemDetails.js
+++ src/menuItemDetails.js
@@ -6,11 +6,16 @@ var MenuItemDetails = Backbone.View.extend({
 	initialize: function(){
 		this.render();
 	},
-
+	template: Handlebars.compile(
+		'<div>' +
+		'<h1>{{name}}</h1>' +
+		'<p><span class="label">{{category}}</span></p>' +
+		'<img src="{{imagepath}}" class="img-polaroid" />' +
+		'</div>'
+	),
 
 	render: function(){
-
-		this.$el.html(this.model.get('category'));
+		this.$el.html(this.template(this.model.attributes));
 		return this; 
 	}
 });
